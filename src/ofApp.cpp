f9a114b427a34ae998e1a34f30e65976d74afc01
Benjamin Goosman <bgoosman@gmail.com>
2018-02-24 15:40:09 -0800

    feat(): added audio / video stutter and reverse

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -9,6 +9,10 @@ void ofApp::setup(){
     sampleRate = 44100;
     audioBufferSize = 512;
     frequency = 440;
+    ofxMaxiSettings::setup(sampleRate, 2, audioBufferSize);
+    mySample.load(ofToDataPath("forward arrows.wav"));
+    backwardSample.load(ofToDataPath("backward arrows.wav"));
+    currentSample = &mySample;
     ofSoundStreamSetup(2, 0, this, sampleRate, audioBufferSize, 4);
 
     ableton.setupLink(beatsPerMinute, 8.0);
@@ -17,7 +21,7 @@ void ofApp::setup(){
     const float desiredHeight = ofApp::height;
     playModes.setup(ofxBenG::playmodes::c920, desiredWidth, desiredHeight, 60);
     ofSetWindowShape(playModes.getVideoWidth(), playModes.getVideoHeight());
-    
+
     beatsPerMinute.addSubscriber([&]() { ableton.setBeatsPerMinute((int)beatsPerMinute); });
 
     propertyBag.add(δ(beatsPerMinute));
@@ -26,7 +30,7 @@ void ofApp::setup(){
     propertyBag.add(δ(stutterLengthBeats));
     propertyBag.add(δ(stutterDelayBeats));
     propertyBag.loadFromXml();
-    
+
     twister = new ofxBenG::twister();
     twister->bindToMultipleEncoders(&propertyBag);
 
@@ -42,35 +46,85 @@ ofApp::~ofApp() {
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    ofxBenG::TimeDiff time = ofGetElapsedTimeMicros();
     ofSetWindowTitle(ofToString(ofGetFrameRate()));
+    float const beat = ableton.getBeat();
     propertyBag.update();
     playModes.update();
 
-    float beat = ableton.getBeat();
     double intPart, fractionalPart;
     fractionalPart = modf(beat, &intPart);
-    if (stutterScheduled && fractionalPart <= 0.05) {
-        if (stutter != nullptr)
-            deleteStutter(beat);
-        createStutter(beat);
-    }
 
-    if (stutter != nullptr) {
-        stutter->update(time, beat);
-        if (stutter->isDone()) {
-            deleteStutter(beat);
+    if (playModes.isInitialized()) {
+        static bool firstTimeSetup = true;
+        if (firstTimeSetup) {
+            renderer.setup(playModes.headers[0]);
+            firstTimeSetup = false;
+        }
+
+        if (fractionalPart <= 0.05 && effectGenerator == nullptr) {
+            effectGenerator = (ofxBenG::beat_action *) new ofxBenG::effect_generator(
+                    beat,
+                    beatsPerMinute,
+                    60.0f,
+                    &playModes.buffers[0],
+                    &playModes.rate,
+                    &renderer,
+                    &mySample,
+                    &backwardSample,
+                    [&](ofxMaxiSample *s) {
+                        this->setCurrentSample(s);
+                    },
+                    &isPlayingTone,
+                    &frequency);
+            effectGenerator->start(beat);
+        }
+
+        if (effectGenerator != nullptr) {
+            effectGenerator->update(beat);
+        }
+
+        if (stutterScheduled && fractionalPart <= 0.05) {
+            if (stutter != nullptr)
+                deleteStutter(beat);
+            createStutter(beat);
+            stutterScheduled = false;
+        }
+
+        if (reverseScheduled && fractionalPart <= 0.05) {
+            if (reverse != nullptr)
+                deleteReverse(beat);
+            createReverse(beat);
+            reverseScheduled = false;
+        }
+
+        if (stutter != nullptr) {
+            stutter->update(beat);
+            if (stutter->isDone(beat)) {
+                deleteStutter(beat);
+            }
+        }
+
+        if (reverse != nullptr) {
+            reverse->update(beat);
+            if (reverse->isDone(beat)) {
+                deleteReverse(beat);
+            }
         }
     }
 }
 
+void ofApp::setCurrentSample(ofxMaxiSample* sample) {
+    this->currentSample = sample;
+}
+
 void ofApp::audioOut(float* output, int bufferSize, int nChannels) {
-    if (isPlayingTone) {
-        for (int i = 0; i < bufferSize; i++) {
-            currentSample = oscillator.sinewave(frequency);
-            output[i * nChannels] = currentSample;
-            output[i * nChannels + 1] = currentSample;
-        }
+    for (int i = 0; i < bufferSize; i++) {
+        float mix = 0;
+        mix += currentSample->playOnce();
+        if (isPlayingTone)
+            mix += oscillator.sinewave(frequency);
+        output[nChannels * i] = mix;
+        output[nChannels * i + 1] = mix;
     }
 }
 
@@ -78,15 +132,13 @@ void ofApp::draw(){
     ofBackground(0);
 
     if (playModes.isInitialized()) {
-        playModes.drawColumns(1);
+//        playModes.drawColumns(1);
 
-        for (int i = 0; i < playModes.getBufferCount(); i++) {
-            syphon[i].publishTexture(&playModes.getBufferTexture(i));
-        }
+//        for (int i = 0; i < playModes.getBufferCount(); i++) {
+//            syphon[i].publishTexture(&playModes.getBufferTexture(i));
+//        }
 
-        if (stutter != nullptr) {
-            renderer.draw(0, 0, ofGetWidth(), ofGetHeight());
-        }
+        renderer.draw(0, 0, ofGetWidth(), ofGetHeight());
     }
 
     if (!inFullscreen) {
@@ -101,12 +153,23 @@ void ofApp::draw(){
 }
 
 void ofApp::createStutter(float beat) {
-    log(beat, "creating new stutter");
-    stutter = new ofxBenG::stutter(beat, stutterDelayBeats, recordLengthBeats, stutterLengthBeats,
-            beatsPerMinute, 60.0f, stutterTimes, &playModes.buffers[0], &playModes.rate,
-            &isPlayingTone, &frequency);
-    stutterScheduled = false;
-    renderer.setup(*stutter->getHeader());
+//    log(beat, "creating new stutter");
+//    ofxBenG::stutter* s = new ofxBenG::stutter(
+//            beat,
+//            recordLengthBeats,
+//            stutterLengthBeats,
+//            beatsPerMinute,
+//            60.0f,
+//            (int)stutterTimes,
+//            &playModes.buffers[0],
+//            &playModes.rate,
+//            &mySample,
+//            &isPlayingTone,
+//            &frequency);
+//    stutterScheduled = false;
+//    renderer.setup(*s->getHeader());
+//    stutter = (ofxBenG::beat_action*) s;
+//    stutter->start(beat);
 }
 
 void ofApp::deleteStutter(float beat) {
@@ -115,42 +178,67 @@ void ofApp::deleteStutter(float beat) {
     stutter = nullptr;
 }
 
+void ofApp::createReverse(float beat) {
+//    log(beat, "creating new reverse");
+//    ofxBenG::reverse* r = new ofxBenG::reverse(
+//            beat,
+//            recordLengthBeats,
+//            stutterLengthBeats,
+//            beatsPerMinute,
+//            60.0f,
+//            &playModes.buffers[0],
+//            &playModes.rate,
+//            &mySample,
+//            &backwardSample,
+//            [&](ofxMaxiSample* s) {this->setCurrentSample(s);},
+//            &isPlayingTone,
+//            &frequency);
+//    renderer.setup(*r->getHeader());
+//    reverse = (ofxBenG::beat_action*) r;
+//    reverse->start(beat);
+}
+
+void ofApp::deleteReverse(float beat) {
+    log(beat, "deleteReverse");
+    delete reverse;
+    reverse = nullptr;
+}
+
 void ofApp::log(float beat, const string &message) const {
     cout << beat << ": " << message << endl;
 }
 
-//--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::keyReleased(int key){
     if (key == 'f') {
         ofToggleFullscreen();
         inFullscreen = !inFullscreen;
     }
-    if (key == ' ') {
+
+    if (key == 's') {
         stutterScheduled = true;
+    }
+
+    if (key == 'r') {
+        reverseScheduled = true;
     }
 }
 
-//--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -5,6 +5,7 @@
 #include "ofxSyphon.h"
 #include "ofxMaxim.h"
 #include "ofxPS3EyeGrabber.h"
+#include "ofxMaxim.h"
 
 class ofApp : public ofBaseApp {
 
@@ -28,6 +29,7 @@ public:
 	void audioOut(float* output, int bufferSize, int nChannels);
 
 	// App
+	void setCurrentSample(ofxMaxiSample* sample);
     ofxBenG::ableton ableton;
     ofxBenG::osc osc = {"10.1.10.202", 1234};
     ofxBenG::twister* twister;
@@ -39,20 +41,28 @@ public:
     ofxBenG::property<float> recordLengthBeats = {"recordLengthBeats", 0.25, 0.025, 4};
     ofxBenG::property<float> stutterLengthBeats = {"stutterLengthBeats", 0.25, 0.025, 4};
     ofxBenG::property<float> stutterDelayBeats = {"stutterDelayBeats", 0.0, 0.0, 8.0};
-	ofxBenG::stutter* stutter = nullptr;
+	ofxBenG::beat_action* stutter = nullptr;
+	ofxBenG::beat_action* reverse = nullptr;
+	ofxBenG::beat_action* effectGenerator = nullptr;
     ofxPm::BasicVideoRenderer renderer;
     static float constexpr width = 1280;
     static float constexpr height = width / (1920.0/1080.0);
     bool inFullscreen = false;
     bool stutterScheduled = false;
+	bool reverseScheduled = false;
     bool isPlayingTone = false;
 
     // Maximillian
     ofxMaxiOsc oscillator;
-    float frequency, currentSample;
+    float frequency;
     int audioBufferSize, sampleRate;
+	ofxMaxiSample mySample;
+	ofxMaxiSample backwardSample;
+	ofxMaxiSample* currentSample;
 
     void log(float beat, const string &message) const;
     void deleteStutter(float beat);
     void createStutter(float beat);
+	void deleteReverse(float beat);
+	void createReverse(float beat);
 };
